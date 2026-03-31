92dd79ba125dd755ec755742e380c30e9e574293
asutosh29 <asutosh29aaz@gmail.com>
2026-04-01 04:29:35 +0530

    feat: Refactor trpc routes

--- apps/server/trpc/appRouter.ts
+++ apps/server/trpc/appRouter.ts
@@ -1,32 +1,10 @@
-import { publicProcedure, router } from "./trpc";
+import { router } from "./trpc";
 import type { User } from "../types/user";
-import z from "zod";
+import { userRouter } from "./routers/user";
 export type { User };
 
 export const appRouter = router({
-    userList: publicProcedure.query(async ()=>{
-        const users: User[] = [
-            {id: "1", name: "John"},
-            {id: "2", name: "Jane"},
-            {id: "3", name: "Bob"},
-        ]
-        return users
-    }),
-    userById: publicProcedure.input(z.string()).query(async(opts)=>{
-        const {input: id} = opts
-        const user: User = {
-            id, name: "amx",
-        }
-        return user
-    }),
-    userCreate: publicProcedure.input(z.object({name: z.string()})).
-    mutation(async (opts)=>{
-        const {input} = opts
-        const user: User = {
-            id: "4", name: input.name
-        }
-        return user
-    })
+    user: userRouter,
 })
 
 export type AppRouter = typeof appRouter
