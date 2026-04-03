fbacccac49a3c13175fc56ec52a0b00d3034c4ae
asutosh29 <asutosh29aaz@gmail.com>
2026-04-03 23:52:54 +0530

    feat: Test streaming using Lg client

--- apps/agent/src/agents/agent.ts
+++ apps/agent/src/agents/agent.ts
@@ -1,12 +1,28 @@
-import { createAgent } from "langchain";
+import { Annotation, messagesStateReducer } from "@langchain/langgraph";
+import { config } from "dotenv";
+import { BaseMessage, createAgent } from "langchain";
+import z from "zod";
 
 import { moonShotLLM } from "../llms/llm";
 import { SYSTEM_PROMPT } from "../prompts/SYSTEM_PROMPT";
 import { weatherTool } from "../tools/weather";
 
+config({
+  path: "../../.env",
+});
+
+const CustomAgentState = Annotation.Root({
+  messages: Annotation<BaseMessage[]>({
+    reducer: messagesStateReducer,
+    default: () => [],
+  }),
+});
 export const agent = createAgent({
   model: moonShotLLM,
   description: "Simple React Agent with a weather tool",
   systemPrompt: SYSTEM_PROMPT,
   tools: [weatherTool],
+  stateSchema: CustomAgentState,
 });
+
+export type AgentState = typeof CustomAgentState.State;
